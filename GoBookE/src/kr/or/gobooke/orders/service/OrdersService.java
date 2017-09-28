deae2d963493f1a8fff8e944d9648a51e4e02b4e
DESKTOP-QHP5M3B\kosta <legm0117@naver.com>
2017-09-28 19:20:49 +0900

    박주연_OrdersServeImple, OrdersService 추가

--- GoBookE/src/kr/or/gobooke/orders/service/OrdersService.java
+++ GoBookE/src/kr/or/gobooke/orders/service/OrdersService.java
@@ -1,17 +1,10 @@
 package kr.or.gobooke.orders.service;
 
 import java.util.List;
-
-import com.sun.javafx.collections.MappingChange.Map;
+import java.util.Map;
 
 import kr.or.gobooke.orders.domain.Orders;
 
-/**
- * orders에 관한 고객의 요구사항을 반영하는 비즈니스 메소드 선언
- * 
- * @author Park Joo-Yeon
- *
- */
 public interface OrdersService {
 	/** 주문등록 */
 	public void create(Orders order);
@@ -20,5 +13,5 @@ public interface OrdersService {
 	public Orders search(Orders no);
 	
 	/** 주문 전체 조회 */
-	public List<Map<String,String>> listAll();
+	public List<Orders> listAll();
 }
---
+++ GoBookE/src/kr/or/gobooke/orders/service/OrdersServiceImpl.java
@@ -0,0 +1,27 @@
+package kr.or.gobooke.orders.service;
+
+import java.util.List;
+
+import kr.or.gobooke.orders.domain.Orders;
+
+public class OrdersServiceImpl implements OrdersService {
+
+	@Override
+	public void create(Orders order) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Orders search(Orders no) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Orders> listAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

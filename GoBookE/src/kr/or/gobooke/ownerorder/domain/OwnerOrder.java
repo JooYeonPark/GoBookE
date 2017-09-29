97e1266bb0c65d10048decef8b19ee86b0dd1d79
Sujin <kwn01037@naver.com>
2017-09-29 18:32:22 +0900

    OwnerOrder getter&setter추가

--- GoBookE/src/kr/or/gobooke/ownerorder/domain/OwnerOrder.java
+++ GoBookE/src/kr/or/gobooke/ownerorder/domain/OwnerOrder.java
@@ -54,4 +54,68 @@ public class OwnerOrder {
 		this.user_id = user_id;
 	}
 	
+	/** getter & setter */
+	
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public int getQty() {
+		return qty;
+	}
+
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(int totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getUser_id() {
+		return user_id;
+	}
+
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+
+	@Override
+	public String toString() {
+		return "OwnerOrder [no=" + no + ", publisher=" + publisher + ", qty=" + qty + ", bookName=" + bookName
+				+ ", totalPrice=" + totalPrice + ", date=" + date + ", user_id=" + user_id + "]";
+	}
+	
 }
